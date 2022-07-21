e4edfa33b057f9ce2b8df826ea64f62c3ae10741
wonjinrun@gmail.com <wonjinrun@gmail.com>
2022-07-21 13:49:37 +0900

    2층 ecap을 통한 sensor 데이터 취득

---
+++ Debug/9pole_bearing_sensorsignal_ecap.d
@@ -0,0 +1,105 @@
+# FIXED
+
+9pole_bearing_sensorsignal_ecap.obj: ../9pole_bearing_sensorsignal_ecap.c
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/DSP28x_Project.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Cla_typedefs.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Device.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Adc.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_BootVars.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Cla.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Comp.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_CpuTimers.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_DevEmu.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Dma.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_ECan.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_ECap.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_EPwm.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_EQep.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Gpio.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_HRCap.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_I2c.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Mcbsp.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_PieCtrl.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_PieVect.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Spi.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Sci.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_SysCtrl.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Usb.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_XIntrupt.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Examples.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_EPwm_defines.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_I2c_defines.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Dma_defines.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Cla_defines.h
+9pole_bearing_sensorsignal_ecap.obj: C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_DefaultISR.h
+
+../9pole_bearing_sensorsignal_ecap.c:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/DSP28x_Project.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Cla_typedefs.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Device.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Adc.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_BootVars.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Cla.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Comp.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_CpuTimers.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_DevEmu.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Dma.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_ECan.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_ECap.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_EPwm.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_EQep.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Gpio.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_HRCap.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_I2c.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Mcbsp.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_PieCtrl.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_PieVect.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Spi.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Sci.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_SysCtrl.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_Usb.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/headers/include/F2806x_XIntrupt.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Examples.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_EPwm_defines.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_I2c_defines.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Dma_defines.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_Cla_defines.h:
+
+C:/ti/c2000/C2000Ware_MotorControl_SDK_3_03_00_00/c2000ware/device_support/f2806x/common/include/F2806x_DefaultISR.h:
+
